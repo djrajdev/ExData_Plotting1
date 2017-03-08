9eeab0a1ae2002a0b77be1890868e4f8f2169f3a
rajdev <rajdev@nyu.edu>
2017-03-07 21:53:22 -0800

    Adding figure1 code + figure

---
+++ figure1_code.R
@@ -0,0 +1,33 @@
+# @Author: DJ Rajdev
+# @Purpose: Exploratory Data Analysis, Lecture 1, Project 1
+
+# rm(list=ls())
+# setwd(paste('','Users','divyajyotirajdev','Desktop','code', 'exploratory_data_analysis', 'ExData_plotting1',sep='/'))
+
+## Before Starting: if data directory not present in WD download data
+if(!file.exists(file.path(getwd(),'household_power_consumption.txt'))){
+  download.file(url='https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip',
+                destfile=file.path(getwd(),'hw1.zip'))
+  unzip(zipfile = file.path(getwd(),'hw1.zip'), exdir = getwd())
+}
+
+## Read data, assuming system has enough memory
+electricdat <- read.table(file= file.path(getwd(),'household_power_consumption.txt'),
+                          header=T, sep=';', na.strings = '?', 
+                          stringsAsFactors = F, 
+                          colClasses = c(rep('character',2), rep('numeric',7)))
+electricdat$Time <- strptime(paste(electricdat$Date,electricdat$Time), 
+                             format = '%d/%m/%Y %H:%M:%S')
+electricdat$Date <- as.Date(electricdat$Date, format = '%d/%m/%Y')
+# Subset to required 2 days
+start <- as.Date('2007-02-01', format='%Y-%m-%d')
+end <- as.Date('2007-02-02', format='%Y-%m-%d')
+electricdat <- electricdat[electricdat$Date == start | electricdat$Date == end,]
+
+
+## Plotting begins
+png('figure1.png', width = 480, height=480)
+hist(electricdat$Global_active_power, breaks=12,
+     col='red', xlab = 'Global Active Power (kilowatts)',
+     ylab= 'Frequency', main = 'Global Active Power')
+dev.off()
